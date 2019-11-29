e23040b273d4ba79f332c31c968df031762638c9
culiutudousi <chenzhicheng1994@gmail.com>
2019-11-29 16:59:49 +0800

    add script that can copy links automaticlly from wechat history list window

---
+++ get_article_links_from_wechat_history_list_window.py
@@ -0,0 +1,178 @@
+import time
+import pyautogui
+import pyperclip
+from PIL import Image
+
+
+ITEM_HEIGHT = 115
+SCROLL_NUM = -260
+SCROLL_HEIGHT = 120
+SEPARATOR_LINE_COLOR = [(237, 237, 237), (242, 242, 242)]
+TOP_RIGHT_CORNER_OF_ARTICLE_LIST_IMG = './res/top_right_corner_of_article_list.png'
+LINK_COPY_BUTTON_IMG = './res/link_copy_button.png'
+
+
+pyautogui.FAILSAFE = True
+pyautogui.PAUSE = 0.5
+
+
+def get_list_window():
+    screen_img = pyautogui.screenshot()
+    top_right_w, top_right_h = get_top_right_corner(screen_img,
+                                                    Image.open(TOP_RIGHT_CORNER_OF_ARTICLE_LIST_IMG).convert("RGB"))
+    # print('Top right corner: ', top_right_w, top_right_h)
+    top_left_w, top_left_h = get_not_white(screen_img, top_right_w, top_right_h, -1, 0)
+    # print('Top left corner: ', top_left_w, top_left_h)
+    middle_right_w, middle_right_h = get_not_white(screen_img, top_right_w, top_right_h, 0, 1)
+    middle_right_h += 1
+    # print('Middle right point: ', middle_right_w, middle_right_h)
+    bottom_right_w, bottom_right_h = get_not_white(screen_img, middle_right_w, middle_right_h, 0, 1)
+    # print('Bottom right corner: ', bottom_right_w, bottom_right_h)
+    # return: left, right, top, middle, bottom
+    return top_left_w, top_right_w, top_right_h, middle_right_h, bottom_right_h
+
+
+def get_top_right_corner(screen_img, corner_img):
+    left, top, _, _ = pyautogui.locate(needleImage=corner_img, haystackImage=screen_img)
+    return left, top
+
+
+def get_not_white(img, start_w, start_h, direction_w, direction_h):
+    img_w, img_h = img.size
+    px = img.load()
+    w, h = int(start_w), int(start_h)
+    w_test, h_test = w + direction_w, h + direction_h
+    while 0 <= w_test < img_w and 0 <= h_test < img_h and px[w_test, h_test] == (255, 255, 255):
+        w, h = w_test, h_test
+        w_test += direction_w
+        h_test += direction_h
+    return w, h
+
+
+def scroll_to_top_of_list_window(left, right, middle, bottom):
+    samples = []
+    list_window = pyautogui.screenshot(region=(left, middle, right - left, bottom - middle))
+    px = list_window.load()
+    new_samples = [px[int((left + right) / 2), h] for h in range(bottom - middle)]
+    while new_samples != samples:
+        samples = new_samples
+        pyautogui.scroll(-SCROLL_NUM)
+        list_window = pyautogui.screenshot(region=(left, middle, right - left, bottom - middle))
+        px = list_window.load()
+        new_samples = [px[int((left + right) / 2), h] for h in range(bottom - middle)]
+
+
+def get_separator_lines(img):
+    img_w, img_h = img.size
+    px = img.load()
+    w = img_w // 2
+    separator_lines = [h for h in range(img_h) if px[w, h] in SEPARATOR_LINE_COLOR]
+    return validate_separator_lines(separator_lines)
+
+
+def validate_separator_lines(separators):
+    validated_separators = []
+    for i, s in enumerate(separators):
+        if i == 0 or s - validated_separators[-1] == ITEM_HEIGHT:
+            validated_separators.append(s)
+        else:
+            break
+    return validated_separators
+
+
+def cut_list_2_items(img):
+    img_w, img_h = img.size
+    items = []
+    last_item_top = 0
+    separator_lines = get_separator_lines(img)
+    # print('Separator: ', separator_lines)
+    for sl in separator_lines[0: -1]:
+        items.append(img.crop((0, sl, img_w, sl + ITEM_HEIGHT - 2)))
+        last_item_top = sl
+    sl = separator_lines[-1]
+    if sl + ITEM_HEIGHT == img_h + 1:
+        items.append(img.crop((0, sl, img_w, sl + ITEM_HEIGHT - 2)))
+        last_item_top = sl
+    return items, last_item_top
+
+
+def get_items_from_list_window(left, right, middle, bottom, scroll_times):
+    list_windows = [pyautogui.screenshot(region=(left, middle, right - left, bottom - middle))]
+    items, last_item_top = cut_list_2_items(list_windows[-1])
+    action_history = ['item'] * len(items)
+    actual_scroll_times = 0
+    actual_scroll_distance, expected_scroll_distance = 0, 0
+    while actual_scroll_distance == expected_scroll_distance:
+        for i in range(scroll_times):
+            pyautogui.scroll(SCROLL_NUM, pause=0.3)
+            actual_scroll_times += 1
+            action_history.append('scroll')
+        time.sleep(1.5)
+
+        list_windows.append(pyautogui.screenshot(region=(left, middle, right - left, bottom - middle)))
+        anchor = pyautogui.locate(needleImage=items[-1], haystackImage=list_windows[-1])
+        if anchor is None:
+            items[-1].show()
+            list_windows[-1].show()
+        new_items, new_last_item_top = cut_list_2_items(list_windows[-1].crop((0, anchor.top + ITEM_HEIGHT,
+                                                                               right - left, bottom - middle)))
+        new_last_item_top += anchor.top + ITEM_HEIGHT
+        actual_scroll_distance = last_item_top - anchor.top
+        expected_scroll_distance = scroll_times * SCROLL_HEIGHT
+
+        items += new_items
+        action_history += ['item'] * len(new_items)
+        last_item_top = new_last_item_top
+
+    for i in range(actual_scroll_times):
+        pyautogui.scroll(-SCROLL_NUM, pause=0.3)
+
+    return items, action_history
+
+
+def open_article_and_copy_link(left, right, middle, bottom, items, action_history, mouse_w_in_list_window, mouse_h_in_list_window):
+    article_links = []
+    have_scrolled = True
+    list_window = None
+    for action in action_history:
+        if action == 'scroll':
+            pyautogui.moveTo(x=mouse_w_in_list_window, y=mouse_h_in_list_window, tween=pyautogui.linear)
+            pyautogui.scroll(SCROLL_NUM, pause=0.3)
+            have_scrolled = True
+        elif action == 'item':
+            item = items.pop(0)
+            if have_scrolled:
+                list_window = pyautogui.screenshot(region=(left, middle, right - left, bottom - middle))
+                have_scrolled = False
+            relative_w, relative_h = pyautogui.center(pyautogui.locate(needleImage=item, haystackImage=list_window))
+            item_w = relative_w + left
+            item_h = relative_h + middle
+            pyautogui.leftClick(x=item_w, y=item_h, tween=pyautogui.linear)
+            link_copy_button_w, link_copy_button_h = pyautogui.center(pyautogui.locateOnScreen(LINK_COPY_BUTTON_IMG))
+            pyautogui.leftClick(x=link_copy_button_w, y=link_copy_button_h)
+            article_links.append(pyperclip.paste())
+            pyautogui.keyDown('altleft', pause=0.3)
+            pyautogui.press('f4')
+            pyautogui.keyUp('altleft', pause=0.3)
+    return article_links
+
+
+def get_article_links_from_wechat_history_list_window():
+    left, right, top, middle, bottom = get_list_window()
+    mouse_w_in_list_window = (left + right) / 2
+    mouse_h_in_list_window = middle + 50
+    pyautogui.moveTo(x=mouse_w_in_list_window, y=mouse_h_in_list_window, duration=0.5, tween=pyautogui.linear)
+    scroll_to_top_of_list_window(left, right, middle, bottom)
+
+    scroll_times = (bottom - middle) // SCROLL_HEIGHT - 2
+    items, action_history = get_items_from_list_window(left, right, middle, bottom, scroll_times)
+
+    article_links = open_article_and_copy_link(left, right, middle, bottom, items, action_history,
+                                               mouse_w_in_list_window, mouse_h_in_list_window)
+    return article_links
+
+
+if __name__ == '__main__':
+    article_links = get_article_links_from_wechat_history_list_window()
+    for link in article_links:
+        print(link)
--- wechat_article_downloader.py
+++ wechat_article_downloader.py
@@ -1,250 +1,125 @@
-import datetime
-import os
-from enum import Enum
-from re import findall, sub
-from urllib.request import urlopen
-
-from PIL import Image
-from bs4 import BeautifulSoup, NavigableString
-from docx import Document
-from docx.enum.text import WD_ALIGN_PARAGRAPH
-from docx.oxml.ns import qn
-from docx.oxml.shared import OxmlElement
-from docx.shared import Inches
-from docx.shared import Pt, Cm
+import time
+from download_wechat_article import download_wechat_article_from, write_article_to_docx
+from get_article_links_from_wechat_history_list_window import get_article_links_from_wechat_history_list_window
 
 
-class Singleton(object):
-    _instance = None
-
-    def __new__(cls, *args, **kw):
-        if not cls._instance:
-            cls._instance = super(Singleton, cls).__new__(cls, *args, **kw)
-        return cls._instance
+def url_is_wechat_article(url):
+    return 'mp.weixin.qq.com' in url
 
 
-class Settings(Singleton):
-    IMAGE_PATH = ''
-    FONT = u'微软雅黑'
-    FONT_SIZE = 10.5
-    FIRST_LINE_INDENT = 0.25
-    MAX_IMAGE_WIDTH = 6
-
-
-class ParagraphType(Enum):
-    SEPARATOR = 0
-    TEXT = 1
-    IMAGE = 2
-
-
-class TextFormat:
-
-    def __init__(self, bold=0, alignment_center=0, alignment_right=0, image_width=0):
-        self.bold = bold
-        self.alignment_center = alignment_center
-        self.alignment_right = alignment_right
-        self.image_width = image_width
-
-
-class ArticleParagraph:
-
-    def __init__(self, type, content='', text_format=TextFormat()):
-        self.type = type
-        self.content = content
-        self.text_format = text_format
-
-
-def parse_content(content, parent_text_format=TextFormat(bold=0,
-                                                         alignment_center=0,
-                                                         alignment_right=0)):
-    text_format = TextFormat(bold=parent_text_format.bold,
-                             alignment_center=parent_text_format.alignment_center,
-                             alignment_right=parent_text_format.alignment_right)
-    if not content:
-        return []
-    if isinstance(content, NavigableString):
-        plain_text = content.replace('&nbsp;', '').strip()
-        if plain_text:
-            return [ArticleParagraph(ParagraphType.TEXT, plain_text, text_format)]
-        return []
-    if content.name == 'strong':
-        text_format.bold = 1
-    if 'style' in getattr(content, 'attrs', {}):
-        style = content['style']
-        if 'text-align: justify' in str(style):
-            text_format.alignment_center = 0
-            text_format.alignment_right = 0
-        elif 'text-align: center' in str(style):
-            text_format.alignment_center = 1
-        elif 'text-align: right' in str(style):
-            text_format.alignment_right = 1
-    if content.name == 'img':
-        if 'data-src' in getattr(content, 'attrs', {}):
-            img_file = download_img(content['data-src'])
-            if img_file:
-                if 'style' in getattr(content, 'attrs', {}):
-                    width = findall('width: (\d+?)px', content['style'])
-                    if width and width[0]:
-                        width_inch = float(width[0]) / 72
-                        if width_inch < Settings().MAX_IMAGE_WIDTH:
-                            text_format.image_width = width_inch
-                return [ArticleParagraph(ParagraphType.IMAGE, img_file, text_format)]
-        return [ArticleParagraph(ParagraphType.TEXT,
-                                     "Can not download image from url: {}".format(
-                                         content['data-src']))]
-    parsed_content = []
-    if content.children:
-        for child in content.children:
-            parsed_content += parse_content(child, TextFormat(bold=text_format.bold,
-                                                              alignment_center=text_format.alignment_center,
-                                                              alignment_right=text_format.alignment_right))
-    if parsed_content and (content.name == 'section' or content.name == 'p'):
-        if not parsed_content[-1].type == ParagraphType.SEPARATOR:
-            parsed_content += [ArticleParagraph(ParagraphType.SEPARATOR)]
-    return parsed_content
-
-
-def get_img_format_from(url):
-    lower_url = str(url).lower()
-    img_formats = ['png', 'jpg', 'jpeg', 'gif', 'bmp']
-    for img_format in img_formats:
-        if lower_url.endswith(img_format):
-            return img_format
-    if lower_url.endswith('other'):
-        return 'jpg'
-    separator_index = max(url.find(separator, -6) for separator in '.=')
-    if separator_index > 0:
-        img_format = url[separator_index + 1:]
-        print("Get new image format from image url: {}".format(img_format))
-        return img_format
-    print("Can not get image format from url: \n{}, try to use jpg".format(url))
-    return 'jpg'
-
-
-def download_img(url):
-    img_format = get_img_format_from(url)
-    if img_format:
-        file_name = datetime.datetime.now().strftime('%Y%m%d_%H%M%S%f') + '.' + img_format
-        file_path = os.path.join(Settings().IMAGE_PATH, file_name)
-        with urlopen(url) as url_cache:
-            with open(file_path, 'wb') as img_file:
-                img_file.write(url_cache.read())
-        reread_img = Image.open(file_path)
-        if reread_img.mode != "RGB":
-            reread_img = reread_img.convert('RGB')
-        reread_img.save(file_path)
-        return file_path
-    print("Download image failed from url: \n{}".format(url))
-    return None
-
-
-def get_image_show_width(pix_width):
-    max_pix_width = 600
-    max_show_width = 6.0
-    if pix_width > max_pix_width:
-        return max_show_width
-    return pix_width * max_show_width / max_pix_width
-
-
-def validate_title(title):
-    rstr = r"[\/\\\:\*\?\"\<\>\|\.]"  # '/ \ : * ? " < > | .'
-    new_title = sub(rstr, "_", title)
-    return new_title
-
-
-def download_wechat_article_from(url):
-    settings = Settings()
-
-    with urlopen(url) as fp:
-        html_content = fp.read().decode()
-
-    soup = BeautifulSoup(html_content, "html5lib")
-    soup.prettify()
-
-    title_html = soup.find('h2', 'rich_media_title')
-    title = title_html.string.strip()
-
-    date_html = findall('s="(\d{4}-\d{2}-\d{2})"', str(soup))
-    article_date = ''
-    if date_html:
-        article_date = date_html[0]
-
-    img_path_base = 'images'
-    img_path_article = os.path.join(img_path_base, ('wx ' + validate_title(title)))
-    if not os.path.exists(img_path_base):
-        os.makedirs(img_path_base)
-    if not os.path.exists(img_path_article):
-        os.makedirs(img_path_article)
-    settings.IMAGE_PATH = img_path_article
-
-    content_html = soup.find('div', 'rich_media_content')
-    article_content = []
-    for child in content_html.children:
-        article_content += parse_content(child)
-
-    return {'title': title,
-            'date': article_date,
-            'content': article_content}
-
-
-def write_article_to_docx(article, url):
-    settings = Settings()
-
-    model_name = 'model.docx'
-    if os.path.exists(model_name):
-        document = Document(model_name)
+def download_one_link(url):
+    if url_is_wechat_article(url):
+        try:
+            article = download_wechat_article_from(url)
+            file_name = write_article_to_docx(article, url)
+            return True, file_name
+        except:
+            return False, 'failed'
     else:
-        document = Document()
-        sections = document.sections
-        for section in sections:
-            section.top_margin = Cm(2.54)
-            section.bottom_margin = Cm(2.54)
-            section.left_margin = Cm(1.91)
-            section.right_margin = Cm(1.91)
-    document.styles['Normal'].font.name = settings.FONT
-    document.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), settings.FONT)
-    style = document.styles['Normal']
-    style.font.size = Pt(settings.FONT_SIZE)
+        return False, 'not_wechat'
 
-    current_paragraph = document.add_paragraph(url)
-    current_paragraph.add_run(' (' + article['date'] + ')')
-    paragraph_format = current_paragraph.paragraph_format
-    paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
-    current_paragraph = document.add_paragraph()
-    current_paragraph.add_run(article['title']).bold = True
-    current_paragraph_format = current_paragraph.paragraph_format
-    current_paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+def download_one_link_ui(url):
+    print('===============================================\n')
+    print('Please wait ...\n')
+    success, message = download_one_link(content)
+    print('===============================================\n')
+    if success:
+        print('Success! Saved as {}\n'.format(message))
+    else:
+        if message == 'failed':
+            print('Failed! Please press ENTER to TRY AGAIN.\n')
+        elif message == 'not_wechat':
+            print('The url is not from wechat article, please download it manually!\n')
 
-    current_paragraph = document.add_paragraph()
-    for paragraph in article['content']:
-        if paragraph.type == ParagraphType.TEXT:
-            words = current_paragraph.add_run(paragraph.content)
-            if paragraph.text_format.bold:
-                words.bold = True
-            if paragraph.text_format.alignment_center:
-                current_paragraph.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-            elif paragraph.text_format.alignment_right:
-                current_paragraph.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+
+def download_from_file_ui(file_name):
+    print('===============================================\n')
+    links = []
+    with open(file_name, 'r') as f:
+        for link in f.readlines():
+            link = link.strip()
+            if link:
+                links.append(link)
+    wechat_links = []
+    other_links = []
+    for link in links:
+        if url_is_wechat_article(link):
+            wechat_links.append(link)
+        else:
+            other_links.append(link)
+    print('Get {} links from "links.txt", '.format(len(links)),
+          'where {} are wechat article links, {} are other links.\n'.format(len(wechat_links),
+                                                                            len(other_links)))
+    success_links = []
+    failed_links = []
+    print('Start to download wechat links ...\n')
+    for i, link in enumerate(wechat_links):
+        print('===============================================\n')
+        print('Downloading {} / {}, please wait ...\n'.format(i + 1, len(wechat_links)))
+        success, message = download_one_link(link)
+        if success:
+            print('Success {} / {}! Saved as {}\n'.format(i + 1, len(wechat_links), message))
+            success_links.append([link, message])
+        else:
+            print('Failed {} / {}! I will try it later.'.format(i + 1, len(wechat_links)))
+            failed_links.append([link, ''])
+    print('===============================================\n')
+    print('Finished! {} success, {} fails.\n'.format(len(success_links), len(failed_links)))
+    if failed_links:
+        still_failed_links = []
+        print('===============================================\n')
+        print('Trying to download failed links again ...\n')
+        for i, link in enumerate(failed_links):
+            link = link[0]
+            print('Downloading {} / {}, please wait ...\n'.format(i + 1, len(failed_links)))
+            success, message = download_one_link(link)
+            if success:
+                print('Success {} / {}! Saved as {}\n'.format(i + 1, len(failed_links), message))
+                success_links.append([link, message])
             else:
-                current_paragraph.paragraph_format.first_line_indent = Inches(settings.FIRST_LINE_INDENT)
-        elif paragraph.type == ParagraphType.IMAGE:
-            img_width = Image.open(paragraph.content).width
-            if paragraph.text_format.image_width:
-                show_width = paragraph.text_format.image_width
-            else:
-                show_width = get_image_show_width(img_width)
-            # print('Adding img: ' + paragraph.content)
-            document.add_picture(paragraph.content, width=Inches(show_width))
-            if paragraph.text_format.alignment_center:
-                last_paragraph = document.paragraphs[-1]
-                last_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        elif paragraph.type == ParagraphType.SEPARATOR:
-            current_paragraph = document.add_paragraph()
-    
-    file_name = 'wx ' + validate_title(article['title']) + '.docx'
-    document.save(file_name)
-    return file_name
+                print('Still failed {} / {}!\n'.format(i + 1, len(failed_links)))
+                still_failed_links.append([link, ''])
+        print('===============================================\n')
+        print('Finished! Finally {} success, {} fails.\n'.format(len(success_links),
+                                                                 len(still_failed_links)))
+        failed_links = still_failed_links
+    print('===============================================\n')
+    print('===============================================')
+    print('Success Links')
+    print('-----------------------------------------------')
+    for link in success_links:
+        print(link[0], ', ', link[1])
+    print('===============================================')
+    print('Failed Links')
+    print('-----------------------------------------------')
+    for link in failed_links:
+        print(link[0], ', ', link[1])
+    print('===============================================')
+    print('Not wechat Links')
+    print('-----------------------------------------------')
+    for link in other_links:
+        print(link)
+    print('-----------------------------------------------\n')
+
+
+def get_article_links_from_wechat_history_list_window_ui(file_name, append_or_overwrite='overwrite'):
+    print('===============================================\n')
+    print('Please make sure that the Wechat History List Window is opened, and no other windows covers it!\n')
+    print('Please stop using mouse or keyboard until finish!\n')
+    print('Will start after 3 secs ...\n')
+    time.sleep(3)
+    print('===============================================\n')
+    print('Start! Please STOP using mouse or keyboard until finish!')
+    print('...\n')
+    links = get_article_links_from_wechat_history_list_window()
+    print('Get {} links from wechat history list window!\n'.format(len(links)))
+    print('Writing to {} ...\n'.format(file_name))
+    file_mode = 'w' if append_or_overwrite == 'overwrite' else 'a'
+    with open(file_name, file_mode) as f:
+        for link in links:
+            f.write(link + '\n')
+    print('Finished! Now you can use your mouse and keyboard happily!\n')
 
 
 if __name__ == '__main__':
@@ -258,17 +133,26 @@ if __name__ == '__main__':
     else:
         while 1:
             print('===============================================\n')
-            content = input('Please PASTE the link (url) of a wechat article, or Enter to quit.\n\n')
+            print('Please PASTE the link (url) of a wechat article,\n',
+                  '    or ENTER to quit,\n',
+                  '    or R to read links from "links.txt" and save them,\n',
+                  '    or C to copy links from wechat history list window to "links.txt" (overwrite),\n',
+                  '    or C+ to copy links from wechat history list window to "links.txt" (append),\n',
+                  '    or CR to copy & read & save links (equals C then R)')
+            content = input('\n')
             if len(content) == 0:
                 break
-            try:
-                print('===============================================\n')
-                print('Please wait ...')
-                article = download_wechat_article_from(url=content)
-                file_name = write_article_to_docx(article, url=content)
-                print('===============================================\n')
-                print('Success! Saved as {}\n'.format(file_name))
-            except:
-                print('===================================================\n')
-                print('Failed! Please press ENTER to TRY AGAIN.\n')
-                input()
+            elif len(content) > 2:
+                download_one_link(content)
+            elif content.upper() == 'R':
+                download_from_file_ui('links.txt')
+            elif content.upper() == 'C':
+                get_article_links_from_wechat_history_list_window_ui('links.txt', append_or_overwrite='overwrite')
+            elif content.upper() == 'C+':
+                get_article_links_from_wechat_history_list_window_ui('links.txt', append_or_overwrite='append')
+            elif content.upper() == 'CR' or content.upper() == 'RC':
+                get_article_links_from_wechat_history_list_window_ui('links.txt', append_or_overwrite='overwrite')
+                download_from_file_ui('links.txt')
+            elif content.upper() == 'C+R' or content.upper() == 'RC+':
+                get_article_links_from_wechat_history_list_window_ui('links.txt', append_or_overwrite='append')
+                download_from_file_ui('links.txt')
